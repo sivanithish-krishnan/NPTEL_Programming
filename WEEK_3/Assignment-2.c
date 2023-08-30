e2b4111148b058b8c1a095c178e5593404f093bf
Sivanithish R K <sivanithishkrishnan@gmail.com>
2023-08-30 22:48:13 +0530

    Update Assignment-2.c

--- WEEK_3/Assignment-2.c
+++ WEEK_3/Assignment-2.c
@@ -10,11 +10,6 @@ int main()
 
     /* The number is entered automatically from the test cases and executed */
     /* Write the rest of the code in the box below
-    As the output should exactly match with the output mentioned in the test cases
-    so copy and paste the following printf statements wherever and whichever is applicable
-      printf("The number is 0.");
-      printf("Negative number.");
-      printf("Positive number.");  
     Do not use any other scanf statements */
     if(number>0)
     { printf("Positive number.");
