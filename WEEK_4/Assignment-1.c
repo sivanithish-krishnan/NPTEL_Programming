2455464d0691e8130a0a3a1f3110c0992cf5a105
Sivanithish R K <sivanithishkrishnan@gmail.com>
2023-08-30 22:45:15 +0530

    Update Assignment-1.c

--- WEEK_4/Assignment-1.c
+++ WEEK_4/Assignment-1.c
@@ -9,11 +9,6 @@ int main()
     scanf("%d %d %d", &n1, &n2, &n3); /* where three number are read from the test cases and are stored in the variable n1, n2 and n3 */
 
 /* Complete the program to get the desired output */
-/* Only use the printf statement given below to exactly match your output 
-with the output cases. Change the variable n1 as required.
-
-printf("%d is the smallest number.", n1);    //Copy and paste this printf statement wherever required. 
-
 */
 if (n1<n2)
 { if(n1<n3)
